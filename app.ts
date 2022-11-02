8a38a095945e34772d32c762fa1e3929b5f367de
dana <jisu2433@gmail.com>
2022-11-02 21:35:13 +0900

    ts공부 1일차

---
+++ app.ts
@@ -0,0 +1,16 @@
+function add(n1: number, n2: number, showResult: boolean) {
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number'){
+    //     throw new Error('Incorrect input!');
+    // }
+    if (showResult) {
+        console.log(n1+n2);
+    } else {
+    return n1 + n2;
+    }
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+
+add(number1, number2, printResult);
