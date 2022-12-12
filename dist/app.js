e6098c82d3303873e7cc7453f841d703a8b607ed
dana <jisu2433@gmail.com>
2022-12-12 22:53:14 +0900

    섹션5: 클래스

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,86 @@
 "use strict";
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-    console.log('Clicked!');
-});
-//# sourceMappingURL=app.js.map
+class Department {
+    // Ŭ������ �ʵ� Ű �̸��� ����
+    // private�� Ŭ������ ������ ��ü ���ο����� ������ �� �ִ� �Ӽ��� �Ǿ��ٴ� ��.
+    // protected�� �ܺο��� ������ �� ���� �ϴ� ���ÿ� �⺻ Ŭ������ Ȯ���ϴ� Ŭ���� �������� ������ �� �ְ� ��
+    // �����, Ŭ������ ����Ǹ� ��ü�� �����Ǹ鼭 ����Ǵ� Ŭ������ ����Ͽ� ����� ��� ��ü���� ����Ǵ� �Լ��� ��ü�� ���� �ʱ�ȭ �۾��� ������ �� ����
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        // private name: string;
+        this.employees = [];
+        // this.name = n;
+    }
+    // ������ �޼ҵ�
+    describe() {
+        console.log(`Department (${this.id}): ${this.name}`);
+    }
+    addEmployee(employee) {
+        // validation etc
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+class ITDepartment extends Department {
+    constructor(id, admins) {
+        super(id, 'IT');
+        this.admins = admins;
+        this.admins = admins;
+        // super �⺻ Ŭ������ �����ڸ� ȣ��, ���⼭�� id�� name�� ����
+    }
+}
+class AccountingDepartment extends Department {
+    // ���ʹ� ���� ������ �� �� �Լ��� �޼ҵ带 �����ϴ� �Ӽ�
+    constructor(id, reports) {
+        super(id, 'Accounting');
+        this.reports = reports;
+        this.lastReport = reports[0];
+    }
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error('No report found');
+    }
+    set mostRecentReport(value) {
+        if (!value) {
+            throw new Error('Please pass in a valid value');
+        }
+        this.addReport(value);
+    }
+    addEmployee(name) {
+        if (name === 'Max') {
+            return;
+        }
+        this.employees.push(name);
+    }
+    addReport(text) {
+        this.reports.push(text);
+        this.lastReport = text;
+    }
+    printReports() {
+        console.log(this.reports);
+    }
+}
+// ��ü�� �����ϴ� �۾�
+const it = new ITDepartment('d1', ['Max']);
+it.addEmployee('Max');
+it.addEmployee('Dana');
+// accounting.employees[2] = 'Anna';
+// Ŭ������ private�̾ �����߻�
+it.describe();
+it.name = 'NEW NAME';
+it.printEmployeeInformation();
+const accounting = new AccountingDepartment('d2', []);
+accounting.mostRecentReport = 'Year End Report';
+accounting.addReport('Something went wrong...');
+console.log(accounting.mostRecentReport);
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+accounting.printReports();
+accounting.printEmployeeInformation();
+// const accountingCopy = {name: 'dana', describe: accounting.describe};
+// accountingCopy.describe(); 
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,96 @@
-const buttonn = document.querySelector('button')!;
-
-function add(n1: number, n2: number){
-    if(n1 + n2 > 0) {
-        return n1 + n2;
+class Department {
+    // private name: string;
+    protected employees: string[] = [];
+    // 클래스의 필드 키 이름만 정의
+    // private은 클래스가 생성된 객체 내부에서만 접근할 수 있는 속성이 되었다는 것.
+    // protected은 외부에서 접근할 수 없게 하는 동시에 기본 클래스를 확장하는 클래스 내에서는 접근할 수 있게 함
+    // 예약어, 클래스와 연결되며 객체가 생성되면서 실행되는 클래스에 기반하여 만드는 모든 객체에도 연결되는 함수로 객체에 대한 초기화 작업을 수행할 수 있음
+    constructor(private id: string, public name: string){
+      // this.name = n;
     }
-}
+    // 생성자 메소드
+    describe(this: Department) {
+      console.log(`Department (${this.id}): ${this.name}`);
+    }
 
-function clickHandler(message: string) {
-    console.log('Clicked!' + message);
-}
+    addEmployee(employee: string){
+      // validation etc
+      this.employees.push(employee);
+    }
 
-if(buttonn) {
-    buttonn.addEventListener('click', clickHandler.bind(null));
-}
+    printEmployeeInformation(){
+      console.log(this.employees.length);
+      console.log(this.employees);
+    }
+  }
+
+  class ITDepartment extends Department {
+    constructor(id: string, public admins: string[]){
+      super(id, 'IT');
+      this.admins = admins;
+      // super 기본 클래스의 생성자를 호출, 여기서는 id와 name을 취함
+    }
+  }
+
+  class AccountingDepartment extends Department {
+    private lastReport: string;
+
+    // get, set 로직을 캡슐화하고 속성을 읽거나 설정하려 할 때 실행되어야 하는 추가적인 로직을 추가하는 데 유용
+    get mostRecentReport(){
+      if(this.lastReport){
+        return this.lastReport;
+      }
+      throw new Error('No report found');
+    }
+
+    set mostRecentReport(value: string){
+      if(!value){
+        throw new Error('Please pass in a valid value')
+      }
+      this.addReport(value);
+    }
+    // 게터는 값을 가지고 올 때 함수나 메소드를 실행하는 속성
+    constructor(id: string, private reports: string[]){
+      super(id, 'Accounting');
+      this.lastReport = reports[0];
+    }
+    addEmployee(name: string){
+      if(name === 'Max'){
+        return;
+      }
+      this.employees.push(name);
+    }
+    addReport(text: string){
+      this.reports.push(text);
+      this.lastReport = text;
+    }
+    printReports() {
+      console.log(this.reports);
+    }
+  }
+  
+  // 객체를 생성하는 작업
+  const it = new ITDepartment('d1',['Max']);
+  
+  it.addEmployee('Max');
+  it.addEmployee('Dana');
+  // accounting.employees[2] = 'Anna';
+  // 클래스가 private이어서 에러발생
+  it.describe();
+  it.name = 'NEW NAME';
+  it.printEmployeeInformation();
+   
+  const accounting = new AccountingDepartment('d2', []);
+
+  accounting.mostRecentReport = 'Year End Report';
+  accounting.addReport('Something went wrong...');
+
+  console.log(accounting.mostRecentReport);
+
+  accounting.addEmployee('Max')
+  accounting.addEmployee('Manu')
+
+  accounting.printReports();
+  accounting.printEmployeeInformation();
+  // const accountingCopy = {name: 'dana', describe: accounting.describe};
+  // accountingCopy.describe(); 
